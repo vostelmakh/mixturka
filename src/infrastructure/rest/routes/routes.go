2e441633f464de95491882100bbe8a1b4f2843c9
Vladislav Stelmakh <stelmakh.vlad@outlook.com>
2025-06-11 00:43:06 +0300

    Url to show version

--- src/infrastructure/rest/routes/routes.go
+++ src/infrastructure/rest/routes/routes.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 
@@ -9,6 +11,13 @@ import (
 
 func ApplicationRouter(router *gin.Engine, db *gorm.DB) {
 	v1 := router.Group("/v1")
+
+	v1.GET("/version", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"version": "1.0.1",
+			"service": "Mixturka",
+		})
+	})
 
 	AuthRoutes(v1, adapter.AuthAdapter(db))
 	UserRoutes(v1, adapter.UserAdapter(db))
