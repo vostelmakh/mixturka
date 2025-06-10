235f52cb663326e46031a2da0d9e6aa8b2f2a2e6
Vladislav Stelmakh <stelmakh.vlad@outlook.com>
2025-06-11 00:37:03 +0300

    Add my specs

--- src/infrastructure/rest/routes/routes.go
+++ src/infrastructure/rest/routes/routes.go
@@ -1,17 +1,14 @@
 package routes
 
 import (
-	"github.com/gbrayhan/microservices-go/src/infrastructure/rest/adapter"
 	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 	"gorm.io/gorm"
+
+	"github.com/gbrayhan/microservices-go/src/infrastructure/rest/adapter"
 )
 
 func ApplicationRouter(router *gin.Engine, db *gorm.DB) {
 	v1 := router.Group("/v1")
-
-	v1.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	AuthRoutes(v1, adapter.AuthAdapter(db))
 	UserRoutes(v1, adapter.UserAdapter(db))
